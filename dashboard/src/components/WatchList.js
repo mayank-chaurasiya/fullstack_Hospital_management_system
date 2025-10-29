a49bd36afb73ec5abfe4d94676a5afefd3a05a98
Mayank <mayankchaurasiya66@gmail.com>
2025-10-30 00:43:33 +0530

    Working on Watchlist component

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,12 @@
-import React from "react";
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+import { useState } from "react";
+import {
+  BarChartOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreHoriz,
+} from "@mui/icons-material";
 
 const WatchList = () => {
   return (
@@ -11,12 +19,92 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchlistActions(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowWatchlistActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="up" />
+          )}
+
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          slots={{ transition: Grow }}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          slots={{ transition: Grow }}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          slots={{ transition: Grow }}
+        >
+          <button className="anlys">
+            <BarChartOutlined />
+          </button>
+        </Tooltip>
+        <Tooltip
+          title="More"
+          placement="top"
+          arrow
+          slots={{ transition: Grow }}
+        >
+          <button className="mre">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
